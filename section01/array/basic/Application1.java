8555364acfcfe773cd84f5c4034b8c1f3d34a8bb
Java_Literal <zziny725@icloud.com>
2024-02-02 17:49:14 +0900

    Commit

--- section01/array/basic/Application1.java
+++ section01/array/basic/Application1.java
@@ -8,8 +8,6 @@ public class Application1 {
          * 각 인덱스에 차례대로 1부터 10까지 값을 넣고 출력하세요
          * */
 
-
-
         int[] arr = new int[10];
         for (int i =0, value = 0; i < arr.length; i++) {
 
